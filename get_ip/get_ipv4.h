382503e353e19fdb4b6bc4f8fe9274f6fe44f191
ARTYOM575 <artyomsahakyan575@gmail.com>
2022-12-17 01:32:25 +0400

    ipv4 as class

--- get_ip/get_ipv4.h
+++ get_ip/get_ipv4.h
@@ -1,51 +1,111 @@
 #include <fstream>
 #include <iostream>
 //
-int size_ipv4 = 0;
-void delete_IPv4_txt(std::string num_3 = ""){
-    if(num_3 == "")
-        return;
-    std::string command = "IPv4";
-    command += num_3 + ".txt";
-    system(command.c_str());
+class my_IPv4{
+public:
+    char ipv4[255][16];
+    char number[255][4];
+    short count_ipv4;
+    short count_number;
+    void delete_IPv4_txt(std::string);
+    void create_IPv4_txt(std::string);
+    void get_IP(std::string);
+    my_IPv4();
+private:
+    void get_number();
+    void del_number(std::string);
+    void substr_(std::string);
+};
+
+my_IPv4::my_IPv4() {
+    for (int i = 0 ; i < 255 ;++i) {
+        ipv4[i][0] = '\0';
+        number[i][0] = '\0';
+    }
+    count_ipv4 = 0;
+    count_number = 0;
 }
 
-void print_number(){
+void my_IPv4::del_number(std::string num_3) {
+    if (number[0][0] == '\0')
+        get_number();
+    std::ifstream file("number.txt");
+    std::string line;
+    std::ofstream temp("temp.txt");
+    if(!file.is_open() || !temp.is_open()) {
+        printf("try again");
+        return;
+    }
+    while (getline(file,line)) {
+        if(line != num_3) {
+            temp << line;
+            temp << "\n";
+        }
+    }
+    system("rm number.txt");
+    system("mv temp.txt number.txt");
+}
+
+void my_IPv4::delete_IPv4_txt(std::string num_3 = "") {
+    bool t = false;
+    if (num_3 == ""){
+        printf("penultimate digits of ip were not corrected");
+        return;
+    }
+    if (number[0][0] == '\0')
+        get_number();
+    for (int i = 0; i < count_number;++i){
+        if (num_3 == number[i]){
+            t = true;
+            break;
+        }
+    }
+    if (t){
+        std::string command = "rm IPv4";
+        command += num_3 + ".txt";
+        system(command.c_str());
+        del_number(num_3);
+    }else{
+        printf("file not found");
+    }
+}
+
+void my_IPv4::get_number() {
     std::ifstream file("number.txt");
     std::string word;
+    int i;
     if(!file.is_open()){
         printf("file isn't open");
         return;
     }
+    count_number = 0;
     while(file >> word) {
-        std::cout<< word;
-   }
-   file.close();
-}
-
-bool is_the_number_here(std::string num_3 = ""){
-    std::ifstream file1("number.txt");
-    std::string num;
-    if (!file1.is_open()){
-        printf("number file is'nt open in bool function");
-        return false;
-    }
-    while(getline(file1,num)){
-        if (num == num_3){
-            file1.close();
-            return true;
+        for(i = 0; i < word.length();++i){
+            if(word[i] != ' '){
+                number[count_number][i] = word[i];
+            }
+            
         }
+        number[count_number][i] = '\0';
+        ++count_number;
     }
-    file1.close();
-    return true;
+    file.close();
 }
 
-void create_IPv4_txt(std::string ipv4_start = ""){
+void my_IPv4::create_IPv4_txt(std::string ipv4_start = "") {
+    bool t = true;
     if(ipv4_start == ""){
-
-    }
+        printf("penultimate digits of ip were not corrected");
         return;
-    if (!is_the_number_here()) {
+    }
+    get_number();
+    for (int i = 0; i < count_number;++i) {
+        if(number[i] == ipv4_start){
+            t = false;
+            break;
+        }  
+    }
+    if(t){
         std::string penultimate_3_digits = "echo '";
         penultimate_3_digits += ipv4_start + "' >> number.txt";
         system (penultimate_3_digits.c_str());
@@ -56,16 +116,16 @@ void create_IPv4_txt(std::string ipv4_start = ""){
     system (command.c_str());
 }
 
-void substr_(std::string str,char * ret_s){
+void my_IPv4::substr_(std::string str){
     int j = 14;
     for (;str[j] != ':';++j)
     {
-        ret_s[j - 14] = str[j]; 
+        ipv4[count_ipv4][j - 14] = str[j]; 
     }
-    ret_s[j - 14] = '\0';
+    ipv4[count_ipv4][j - 14] = '\0';
 }
 
-void get_IP(char ipv4[255][16],std::string number = ""){
+void my_IPv4::get_IP(std::string number = ""){
     if (number == ""){
         printf("penultimate digits of ip were not corrected");
         return;
@@ -75,12 +135,13 @@ void get_IP(char ipv4[255][16],std::string number = ""){
     std::string ip_;
     std::ifstream file(file_name);
     if (!file.is_open()){
-        printf("file is'nt open");
+        printf("file isn't open");
+        return;
     }
-    size_ipv4 = 0;
+    count_ipv4 = 0;
     while (getline(file,ip_)){
-        substr_(ip_,ipv4[size_ipv4]);
-        ++size_ipv4;
+        substr_(ip_);
+        ++count_ipv4;
     }
     file.close();  
 }
--- main/main.cpp
+++ main/main.cpp
@@ -4,45 +4,36 @@
 // call function add_IPv4_txt() to create a IP if file exists adds from end . passing the penultimate possible value(s) of host on your local network
 // 192.168.x.y    send function x as a string
 int main(){
-    char ipv4[255][16];
-    for (int i = 0 ; i < 255 ;++i){
-        ipv4[i][0] = '\0';
-    }
+    my_IPv4 Ip;
     int s;
     std::string num_3;
     while (1){
         printf("\n input number action \n 1 create_ip ");
         printf("\n 2 delete ip_file ");
-        printf("\n 3 print ip_number ");
+        printf("\n 3 get ip_number ");
         printf("\n 4 get ip in array");
         printf("\n 5 print ip_array \n");
         std::cin >> s;  
-        switch (s){
+        switch (s > 1){
         case 1:
             std::cout << "3_num" << std::endl;
-            std::cin >> num_3; 
-            create_IPv4_txt(num_3);
+            std::cin >> num_3;
+            Ip.create_IPv4_txt(num_3);
             break;
         case 2:
             std::cout << "3_num" << std::endl;
             std::cin >> num_3;
-            delete_IPv4_txt(num_3);
-            break;
-        case 3:
-            print_number();
+            Ip.delete_IPv4_txt(num_3);
             break;
         case 4:
             std::cout << "3_num" << std::endl;
             std::cin >> num_3; 
-            get_IP(ipv4,num_3);
+            Ip.get_IP(num_3);
             break;
         case 5:
-            std::cout << sizeof(ipv4)/16;
-            for(int i = 0; i < size_ipv4; ++i){
-                std::cout << ipv4[i] << std::endl;
+            for(int i = 0; i < Ip.count_ipv4; ++i){
+                std::cout << Ip.ipv4[i] << std::endl;
             }
-            break;
-        default:
             break;
         }
     }
